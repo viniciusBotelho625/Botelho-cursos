8facbebcb243bfc01d1a14674dc75532db6236cc
viniciusBotelho625 <58275652+viniciusBotelho625@users.noreply.github.com>
2020-10-04 00:55:31 -0300

    Componente cursos

--- cursos/src/app/app.component.spec.ts
+++ cursos/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cursos'`, () => {
+  it(`should have as title 'primeiro-projeto'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cursos');
+    expect(app.title).toEqual('primeiro-projeto');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('cursos app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('primeiro-projeto app is running!');
   });
 });
---
+++ cursos/src/app/list-cursos/list-cursos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-cursos',
+  templateUrl: './list-cursos.component.html',
+  styleUrls: ['./list-cursos.component.css']
+})
+export class ListCursosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
